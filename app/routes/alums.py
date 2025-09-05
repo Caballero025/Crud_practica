b565a13b9467a245ca4db65bb95ccb519826ad65
Caballero025 <l20590234@sjuanrio.tecnm.mx>
2025-09-05 15:52:59 -0600

    Feat: Funcionalidad de agregar alumnos

--- app/routes/alums.py
+++ app/routes/alums.py
@@ -13,4 +13,20 @@ def index():
     return render_template('index.html', alumnos = alumnos)
 
 
+@alu_bp.route('/new',methods=['GET','POST'])
+def create_alumno():
+     if request.method == 'POST':
+         #Agregar Alumno 
+         no_control = request.form['no_control']
+         nombre = request.form['nombre']
+         ap_paterno = request.form['ap_paterno']
+         ap_materno = request.form['ap_materno']
+         semestre = request.form['semestre']
 
+         nvo_alumno = Alumnos(no_control = no_control, nombre = nombre,ap_paterno = ap_paterno, ap_materno = ap_materno, semestre = semestre)
+
+         db.session.add(nvo_alumno)
+         db.session.commit()
+         return redirect(url_for('alumnos.index'))
+     #Aqui sigue si es GET
+     return render_template('create_alumno.html')
